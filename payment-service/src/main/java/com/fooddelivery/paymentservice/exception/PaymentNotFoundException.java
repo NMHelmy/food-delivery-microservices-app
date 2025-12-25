70b5c4ccd719588db89fa85881df3ba6a59833cf
husseinhazemsary <138691996+husseinhazemsary@users.noreply.github.com>
2025-12-25 21:50:07 +0200

    payment

--- payment-service/src/main/java/com/fooddelivery/paymentservice/exception/PaymentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.fooddelivery.paymentservice.exception;
-
-public class PaymentNotFoundException extends RuntimeException {
-
-    public PaymentNotFoundException(String message) {
-        super(message);
-    }
-}
--- payment-service/src/main/java/com/fooddelivery/paymentservice/models/PaymentStatus.java
+++
@@ -1,9 +0,0 @@
-package com.fooddelivery.paymentservice.models;
-
-public enum PaymentStatus {
-    PENDING,
-    SUCCESS,
-    CANCELLED,
-    REFUNDED
-}
-
